72f57a0f6abd2d3ab89a40385b4d3f5eb826eccd
Noriyuki Abe <noriyukipy@gmail.com>
2020-10-21 20:41:17 +0900

    Add tests of fitting for PostLNDecoder

--- tests/small/models_test.py
+++ tests/small/models_test.py
@@ -11,6 +11,8 @@ from tfchat.models import create_padding_mask
 from tfchat.models import create_look_ahead_mask
 from tfchat.models import create_combined_mask
 from tfchat.models import PostLNDecoder
+from tfchat.losses import PaddingLoss
+from tfchat.optimizers import TransformerScheduler
 
 
 def test_position_encoding():
@@ -144,7 +146,7 @@ def test_create_combined_mask():
                                   [[[1, 1, 1], [1, 0, 1], [1, 0, 1]]]])
 
 
-def test_post_ln_decoder():
+def test_PostLNDecoder():
     num_layers = 4
     d_model = 128
     num_heads = 8
@@ -164,3 +166,33 @@ def test_post_ln_decoder():
     got = gpt(inputs, training=False)
 
     assert got.shape == (batch_size, seq_len, vocab_size)
+
+
+def test_PostLNDecoder_fit():
+    num_layers = 4
+    d_model = 128
+    num_heads = 8
+    d_ff = 256
+    vocab_size = 1000
+    max_position_encoding = 100
+
+    model = PostLNDecoder(num_layers=num_layers, d_model=d_model,
+                          num_heads=num_heads,
+                          d_ff=d_ff, vocab_size=vocab_size,
+                          max_position_encoding=max_position_encoding)
+    model.compile(
+        loss=PaddingLoss(),
+        optimizer=keras.optimizers.Adam(TransformerScheduler(d_model),
+                                        beta_1=0.9,
+                                        beta_2=0.98,
+                                        epsilon=1e-9),
+    )
+
+    # Prepare input data
+    seq_len = 10
+    num_samples = 10
+
+    inputs = np.ones((num_samples, seq_len), dtype=np.int32)
+    outputs = inputs
+
+    model.fit(inputs, outputs)
