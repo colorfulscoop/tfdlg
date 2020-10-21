73934071763be58588094ff4c6c635175f340298
Noriyuki Abe <noriyukipy@gmail.com>
2020-10-21 19:29:10 +0900

    Add README and setup.py

---
+++ README.md
@@ -0,0 +1,12 @@
+# TfChat
+
+TfChat provides a **t**rans**f**ormer-based response generation models with **T**ensor**F**low for **chat**bots.
+
+
+## Installation
+
+Prepare Python >= 3.8 first. Then run `pip` to install this package from GitHub.
+
+```sh
+$ pip install git+https://github.com/noriyukipy/tfchat
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="tfchat",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        # Install tensorflow 2.X
+        "tensorflow~=2.0",
+        # "tensorflow_text~=2.0"
+        # "tensorflow-hub",
+        # Install Scikit-learn
+        # "scikit-learn>=0.22"
+    ],
+    version="0.1.0",
+    author="Noriyuki Abe",
+)
