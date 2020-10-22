b2c78649a46d2e08c8d4bc6a6763f1ea9b207143
Noriyuki Abe <noriyukipy@gmail.com>
2020-10-22 18:58:28 +0900

    Add configs for GPT2

--- tfchat/configs.py
+++ tfchat/configs.py
@@ -23,3 +23,43 @@ class TransformerConfig(BaseModel):
     attention_dropout_rate: float = 0.1
     residual_dropout_rate: float = 0.1
     epsilon: float = 1e-6
+
+
+# ===== Config for GPT2 =====
+
+
+class GPT2SmallConfig(BaseModel):
+    """Config for GPT2 small model with 117M parameters"""
+    num_layers: int = 12
+    d_model: int = 768
+    num_heads: int = 12
+    d_ff: int = 3072  # == 4 * d_model
+    vocab_size: int = 50257
+    max_position_encoding: int = 1024
+    attention_dropout_rate: float = 0.1
+    residual_dropout_rate: float = 0.1
+    epsilon: float = 1e-6
+
+
+class GPT2MediumConfig(GPT2SmallConfig):
+    """Config for GPT2 medium model with 345 parameters"""
+    num_layers: int = 24
+    d_model: int = 1024
+    num_heads: int = 16
+    d_ff: int = 4096
+
+
+class GPT2LargeConfig(GPT2SmallConfig):
+    """Config for GPT2 large model with 774 parameters"""
+    num_layers: int = 36
+    d_model: int = 1280
+    num_heads: int = 20
+    d_ff: int = 5120
+
+
+class GPT2XLConfig(GPT2SmallConfig):
+    """Config for GPT2 XL model with 1558 parameters"""
+    num_layers: int = 48
+    d_model: int = 1600
+    num_heads: int = 25
+    d_ff: int = 6400
