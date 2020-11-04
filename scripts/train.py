8d4d552268af64285e5e2536380d10c24ee7bd6e
Noriyuki Abe <noriyukipy@gmail.com>
2020-11-04 23:48:04 +0900

    Update train script

--- scripts/train.py
+++ scripts/train.py
@@ -6,16 +6,24 @@ from tfchat.generations import TopKTopPGenerator
 from tfchat.utils import import_class
 from tfchat.utils import save_model
 from tfchat.utils import load_model
+from tfchat.tokenizers import SentencePieceTokenizer
 
 import tensorflow.keras as keras
 import numpy as np
 
 
-def main(save_model_dir=None, load_model_dir=None, do_eval=True,
-         batch_size=2, epochs=1,
-         model_cls="tfchat.models.PreLNDecoder",
-         config_cls="tfchat.configs.Config"):
-    # Prepare save and load
+def main(tokenizer_model_dir, load_model_dir=None,
+         do_train=True, do_eval=True, do_generate=False,
+         # Parameters for do_generate
+         max_len=20,
+         # Parameters for training
+         train_file=None, valid_file=None, save_model_dir=None, batch_size=2, epochs=1,
+         model_cls="tfchat.models.PreLNDecoder", config_cls="tfchat.configs.Config",
+         ):
+    # Load tokenizer
+    tokenizer = SentencePieceTokenizer.load(model_dir=tokenizer_model_dir)
+
+    # Prepare model
     if load_model_dir:
         model, config = load_model(load_model_dir)
     else:
@@ -26,47 +34,66 @@ def main(save_model_dir=None, load_model_dir=None, do_eval=True,
                             vocab_size=100, context_size=64,
                             attention_dropout_rate=0.1, residual_dropout_rate=0.1,
                             embedding_dropout_rate=0.1, epsilon=1e-06)
+        # Override the vocab_size with the number of tokens in tokenizer
+        config.vocab_size = len(tokenizer)
+
         model = model_cls(config)
         model.build(input_shape=(None, config.context_size))
-        model.summary()
+
+    print("Model config:", config)
+    model.summary()
 
     # Prepare dataset
-    train_ids = np.tile(np.arange(10, dtype=np.int32), 1000)  # Prepare token ids for training data
-    valid_ids = np.tile(np.arange(10, dtype=np.int32), 100)   # Prepare token ids for validation data
-
-    dataset = BlockDataset(block_size=config.context_size, batch_size=batch_size)
-    train_dataset = dataset.build(train_ids, shuffle=True)
-    valid_dataset = dataset.build(valid_ids, shuffle=False)
-
-    # Prepare model
-    num_steps = len([_ for _ in train_dataset])
-    schedule = WarmupLinearDecay(max_learning_rate=1e-4, warmup_steps=0, training_steps=num_steps*epochs)
-    optimizer = keras.optimizers.Adam(schedule, beta_1=0.9, beta_2=0.999, epsilon=1e-8, clipnorm=1.0)
-    model.compile(loss=PaddingLoss(), optimizer=optimizer)
+    if do_train or do_eval:
+        assert train_file and valid_file
+        dataset = BlockDataset(block_size=config.context_size, batch_size=batch_size)
+        train_dataset = dataset.from_text_generator(lambda: (t.strip("\n") for t in open(train_file)), encode_fn=tokenizer.encode, shuffle=True)
+        valid_dataset = dataset.from_text_generator(lambda: (t.strip("\n") for t in open(valid_file)), encode_fn=tokenizer.encode, shuffle=False)
 
     # Train
-    history = model.fit(
-        train_dataset,
-        validation_data=valid_dataset,
-        epochs=epochs,
-        callbacks=[
-            keras.callbacks.EarlyStopping(patience=1, restore_best_weights=True),
-            # If you want to save chekcpoints, remove the next comment out
-            #keras.callbacks.ModelCheckpoint("keras_model/", save_best_only=True)
-        ]
-    )
-    if save_model_dir:
-        save_model(save_model_dir, model, config)
+    if do_train:
+        # Prepare model
+        num_steps = len([_ for _ in train_dataset])
+        print("Num steps per epoch:", num_steps)
+
+        schedule = WarmupLinearDecay(max_learning_rate=1e-4, warmup_steps=0, training_steps=num_steps*epochs)
+        optimizer = keras.optimizers.Adam(schedule, beta_1=0.9, beta_2=0.999, epsilon=1e-8, clipnorm=1.0)
+        model.compile(loss=PaddingLoss(), optimizer=optimizer)
+        history = model.fit(
+            train_dataset,
+            validation_data=valid_dataset,
+            epochs=epochs,
+            callbacks=[
+                keras.callbacks.EarlyStopping(patience=1, restore_best_weights=True),
+                # If you want to save chekcpoints, remove the next comment out
+                #keras.callbacks.ModelCheckpoint("keras_model/", save_best_only=True)
+            ]
+        )
+        if save_model_dir:
+            save_model(save_model_dir, model, config)
 
     # Evaluate
     if do_eval:
         ppl = perplexity(model, valid_dataset)
         print("Validation PPL:", ppl)
 
-    # Generate
-    gen = TopKTopPGenerator(model=model, max_len=3)
-    inputs = np.array([[1, 2, 3, 4, 5]], dtype=np.int32)
-    print(gen.generate(inputs))
+    if do_generate:
+        # Generate
+        gen = TopKTopPGenerator(model=model, max_len=max_len)
+        while True:
+            try:
+                text = input(">>> ")
+            except (KeyboardInterrupt, EOFError):
+                print("")
+                print("Bye")
+                break
+            ids = tokenizer.encode(text)
+            output_ids = gen.generate(np.array([ids], dtype=np.int32))
+            tkns = tokenizer.decode(output_ids.tolist())
+            print("Input: ", text)
+            print("Encode:", ids)
+            print("Gen:   ", output_ids)
+            print("Decode:", tkns)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -5,13 +5,9 @@ setuptools.setup(
     name="tfchat",
     packages=setuptools.find_packages(),
     install_requires=[
-        # Install tensorflow 2.X
         "tensorflow~=2.0",
+        "sentencepiece==0.1.91",
         "scipy~=1.5.0",
-        # "tensorflow_text~=2.0"
-        # "tensorflow-hub",
-        # Install Scikit-learn
-        # "scikit-learn>=0.22"
         "pydantic==1.6.1",
     ],
     version="0.1.0",
