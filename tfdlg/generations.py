20b48d773edc8f62e7c49b647a4eeb16692892d6
Noriyuki Abe <noriyukipy@gmail.com>
2021-01-09 22:53:11 +0900

    Implement stop_id for TopKTopPGenerator

--- tfdlg/generations.py
+++ tfdlg/generations.py
@@ -90,12 +90,13 @@ def sample_multinomial(dist):
 class TopKTopPGenerator:
     """Sentence generator to sandom sampling from top-k distribution"""
     def __init__(self, model, top_k=50, top_p=0.95,
-                 bad_ids=[], max_len=20):
+                 bad_ids=[], max_len=20, stop_id=None):
         self._model = model
         self._top_p = top_p
         self._top_k = top_k
         self._bad_ids = bad_ids
         self._max_len = max_len
+        self._stop_id = stop_id
 
     def step(self, inputs):
         """
@@ -121,8 +122,14 @@ class TopKTopPGenerator:
         return sample_multinomial(filtered_dist)
 
     def generate(self, inputs):
+        need_stop = np.array([False for _ in range(inputs.shape[0])])
+
         for _ in range(self._max_len):
             outputs = self.step(inputs)
+            need_stop = need_stop | (outputs == self._stop_id)
+            if self._stop_id is not None and np.all(need_stop):
+                break
+
             outputs = outputs.reshape((inputs.shape[0], 1))
             inputs = np.concatenate([inputs, outputs], axis=-1)
         return inputs
