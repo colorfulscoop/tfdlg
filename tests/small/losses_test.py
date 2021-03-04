33d8667a49ea9ca8cf51c84f5b0c16deec719904
Noriyuki Abe <noriyukipy@gmail.com>
2021-03-04 17:34:42 +0900

    Modify PaddingLoss to work when all the inputs are masked

--- tests/small/losses_test.py
+++ tests/small/losses_test.py
@@ -3,9 +3,19 @@ from tfdlg.losses import PaddingLoss
 
 
 def test_padding_loss():
-    gold = np.array([1, 0, 0], dtype=np.int32)
+    gold = np.array([1, 1, 0], dtype=np.int32)
     pred = np.array([[1, 0], [1, 0], [1, 0]], dtype=np.float32)
 
     loss_fn = PaddingLoss()
     got = loss_fn(gold, pred)  # == <tf.Tensor: shape=(), dtype=float32, numpy=1.3132616>
     np.testing.assert_almost_equal(got, 1.3132616)
+
+
+def test_padding_loss_allmask():
+    """Test when all the output labels are masked"""
+    gold = np.array([0, 0, 0], dtype=np.int32)
+    pred = np.array([[1, 0], [1, 0], [1, 0]], dtype=np.float32)
+
+    loss_fn = PaddingLoss()
+    got = loss_fn(gold, pred)  # == <tf.Tensor: shape=(), dtype=float32, numpy=1.3132616>
+    np.testing.assert_almost_equal(got, 0)
--- tfdlg/losses.py
+++ tfdlg/losses.py
@@ -11,6 +11,11 @@ class PaddingLoss(keras.losses.Loss):
         )
         self._from_logits = from_logits
 
+    # __call__ needs to be wrapped by tf.function because it uses
+    # `if` statement. If not wrippared, tf.cond, (or tf.function for more convenience)
+    # should be used.
+    # Find more details here https://github.com/tensorflow/tensorflow/issues/40916#issuecomment-660305407
+    @tf.function
     def __call__(self, y_true, y_pred, sample_weight=None):
         mask = tf.math.logical_not(tf.math.equal(y_true, 0))
         loss = self._loss_fn(y_true, y_pred)
@@ -18,7 +23,13 @@ class PaddingLoss(keras.losses.Loss):
         mask = tf.cast(mask, dtype=loss.dtype)
         # Apply mask
         loss = loss * mask
-        return tf.reduce_sum(loss) / tf.reduce_sum(mask)
+
+        # If all the target value is masked, return 0.
+        mask_sum = tf.reduce_sum(mask)
+        if mask_sum == 0:
+            return tf.reduce_sum(loss)  # should be equal to 0
+        else:
+            return tf.reduce_sum(loss) / mask_sum
 
     def get_config(self):
         base_config = super().get_config()
