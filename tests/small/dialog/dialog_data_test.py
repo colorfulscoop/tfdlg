2a1fd310092494ca3d43af98bed85afda6a22057
Noriyuki Abe <noriyukipy@gmail.com>
2021-03-06 10:19:47 +0900

    Implement DialogClsDataset and modify interface from from_text_generator to from_generator

--- tests/small/dialog/dialog_data_test.py
+++ tests/small/dialog/dialog_data_test.py
@@ -1,15 +1,13 @@
 from tfdlg.dialog.data import DialogDataset
+from tfdlg.dialog.data import DialogClsDataset
+from tfdlg.dialog.tokenizers import encode_dialog
 import numpy as np
-from tfdlg.data import LineByLineDataset
-from tfdlg.data import BlockDataset
 
 
 def test_DialogDataset_test_generator():
     def encode(text):
         words = text.split(" ")
         return [int(w) for w in words]
-
-    loader = DialogDataset(max_len=5, encode_fn=encode, sep_token_id=-1)
 
     texts = [["1", "1 2"],
              ["1 2", "1 2 3"],
@@ -18,7 +16,13 @@ def test_DialogDataset_test_generator():
              ["1"],
              ]
 
-    dataset = loader.from_text_generator(lambda: texts, batch_size=2, shuffle=False)
+    dataset = DialogDataset.from_generator(
+        lambda: texts,
+        context_encode_fn=lambda context: encode_dialog(encode_fn=encode, sep_token_id=-1, context=context, response=None),
+        max_len=5,
+        batch_size=2,
+        shuffle=False
+    )
 
     got_X = np.array([item[0].numpy() for item in dataset])
     want_X = np.array([[[-1, 1, -1, 1], [-1, 1, 2, -1]],
@@ -32,96 +36,62 @@ def test_DialogDataset_test_generator():
     np.testing.assert_equal(got_y, want_y)
 
 
-def test_DialogDataset_convert_text_to_ids():
+def test_DialogClsDataset_test_generator():
     def encode(text):
         words = text.split(" ")
         return [int(w) for w in words]
 
-    dt = DialogDataset(max_len=8, encode_fn=encode, sep_token_id=-1)
+    texts = [["1", "1 2"],
+             ["1 2", "1 2 3"],
+             ["1 2 3 4 5", "1 2 3 4 5 6"],
+             ["1 2"],
+             ["1"],
+             ]
 
-    # Case: context only
-    got = dt.convert_text_to_ids(text="|0 1|3|")
-    want = np.array([-1, 0, 1, -1, 3, -1], dtype=np.int32)
-    np.testing.assert_equal(got, want)
+    dataset = DialogClsDataset.from_generator(
+        lambda: texts,
+        encode_fn=encode,
+        max_len=5,
+        sep_token_id=-1,
+        batch_size=2,
+        shuffle=False,
+        seed=0,
+    )
 
-    # Case: many tokens more than max_len
-    got = dt.convert_text_to_ids(text="|0 1 2 3 4|5 6 7|")
-    want = np.array([-1, 0, 1, 2, 3, 4, -1, 5], dtype=np.int32)
-    np.testing.assert_equal(got, want)
+    got = list(dataset.as_numpy_iterator())
+    print(got)
 
-    # Case: context and response
-    got = dt.convert_text_to_ids("|0|3|5 6")
-    want = np.array([-1, 0, -1, 3, -1, 5, 6], dtype=np.int32)
-    np.testing.assert_equal(got, want)
+    got_lm_X = np.array([item[0][0] for item in got])
+    want_lm_X = np.array([[[-1, 1, -1, 1, 2], [-1, 1, 2, -1, 1]],
+                          [[-1, 1, 2, 3, 4], [-1, 1, 2, -1, 0]],
+                          [[-1, 1, -1, 0, 0], [-1, 1, -1, 1, 2]],
+                          [[-1, 1, 2, -1, 1], [-1, 1, 2, 3, 4]],
+                          [[-1, 1, -1, 0, 0], [-1, 1, 2, -1, 0]],
+                          ])
 
-    # Case: resposne only
-    got = dt.convert_text_to_ids(text="|5 6")
-    want = np.array([-1, 5, 6], dtype=np.int32)
-    np.testing.assert_equal(got, want)
+    got_lm_y = np.array([item[1][0] for item in got])
+    want_lm_y = np.array([[[1, -1, 1, 2, -1], [1, 2, -1, 1, 2]],
+                          [[1, 2, 3, 4, 5], [1, 2, -1, 0, 0]],
+                          [[1, -1, 0, 0, 0], [1, -1, 1, 2, 3]],
+                          [[1, 2, -1, 1, 2], [1, 2, 3, 4, 5]],
+                          [[1, -1, 0, 0, 0], [1, 2, -1, 0, 0]],
+                          ])
+    got_cls_ids = np.array([item[0][1] for item in got])
+    want_cls_ids = np.array([[4, 4],
+                             [4, 3],
+                             [2, 4],
+                             [4, 4],
+                             [2, 3],
+                             ])
+    got_label = np.array([item[1][1] for item in got])
+    want_label = np.array([[1, 1],
+                           [1, 1],
+                           [1, 0],
+                           [0, 0],
+                           [0, 0],
+                           ])
 
-    # Case: Nothing given
-    got = dt.convert_text_to_ids(text="")
-    want = np.array([], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-    # Case: separator only give
-    got = dt.convert_text_to_ids(text="|")
-    want = np.array([-1], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-
-def test_DialogDataset_convert_context_to_ids():
-    def encode(text):
-        words = text.split(" ")
-        return [int(w) for w in words]
-
-    dt = DialogDataset(max_len=8, encode_fn=encode, sep_token_id=-1)
-
-    # Case: context only
-    got = dt.convert_context_to_ids(context=["0 1", "3"])
-    want = np.array([-1, 0, 1, -1, 3, -1], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-    # Case: many tokens more than max_len
-    got = dt.convert_context_to_ids(context=["0 1 2 3 4", "5 6 7"])
-    want = np.array([-1, 0, 1, 2, 3, 4, -1, 5], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-    # Case: context and response
-    got = dt.convert_context_to_ids(context=["0", "3"], response="5 6")
-    want = np.array([-1, 0, -1, 3, -1, 5, 6], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-    # Case: resposne only
-    got = dt.convert_context_to_ids(context=[], response="5 6")
-    want = np.array([-1, 5, 6], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-    # Case: Nothing given
-    got = dt.convert_context_to_ids(context=[])
-    want = np.array([-1], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-
-def test_BlockDataset_convert_context_to_ids():
-    def encode(text):
-        words = text.split(" ")
-        return [int(w) for w in words]
-
-    dt = BlockDataset(block_size=4, encode_fn=encode)
-
-    got = dt.convert_context_to_ids(context=["0 1", "3"], response="4 5")
-    want = np.array([0, 1, 3, 4, 5], dtype=np.int32)
-    np.testing.assert_equal(got, want)
-
-
-def test_LineByLineDataset_convert_context_to_ids():
-    def encode(text):
-        words = text.split(" ")
-        return [int(w) for w in words]
-
-    dt = LineByLineDataset(max_len=4, encode_fn=encode)
-
-    got = dt.convert_context_to_ids(context=["0 1", "3"], response="4 5")
-    want = np.array([0, 1, 3, 4], dtype=np.int32)
-    np.testing.assert_equal(got, want)
+    np.testing.assert_equal(got_lm_X, want_lm_X)
+    np.testing.assert_equal(got_lm_y, want_lm_y)
+    np.testing.assert_equal(got_label, want_label)
+    np.testing.assert_equal(got_cls_ids, want_cls_ids)
--- tfdlg/data.py
+++ tfdlg/data.py
@@ -4,24 +4,25 @@ from typing import List
 
 
 class BlockDataset:
-    def __init__(self, block_size, encode_fn):
-        self._block_size = block_size
-        self._encode_fn = encode_fn
-
-    def from_text_generator(self, generator, batch_size=1, buffer_size=10000, shuffle=False):
+    @classmethod
+    def from_generator(cls, generator, encode_fn, block_size, batch_size=1, buffer_size=10000, shuffle=False):
         """
         Args:
             generator: callable object which returns a generator function.
                 The generator functions returns str for each time.
             encode_fn: function which takes str as an input and returns list of integers (i.e. List[int])
         """
-        window_length = self._block_size + 1
+        def gen():
+            for text in generator():
+                for id_ in np.array(encode_fn(text), dtype=np.int32):
+                    yield id_
 
-        dataset = tf.data.Dataset.from_generator(self._gen_iter_ids(generator),
+        window_length = block_size + 1
+        dataset = tf.data.Dataset.from_generator(gen,
                                                  output_types=tf.int32,
                                                  output_shapes=tf.TensorShape([]))
         dataset = dataset.window(window_length,
-                                 shift=self._block_size,
+                                 shift=block_size,
                                  drop_remainder=True)
         dataset = dataset.flat_map(lambda wd: wd.batch(window_length))
         if shuffle:
@@ -32,66 +33,15 @@ class BlockDataset:
 
         return dataset
 
-    def convert_text_to_ids(self, text: str):
-        return np.array(self._encode_fn(text), dtype=np.int32)
-
-    def _gen_iter_ids(self, text_generator):
-        def gen():
-            for text in text_generator():
-                for id_ in self.convert_text_to_ids(text):
-                    yield id_
-        return gen
-
-    def convert_context_to_ids(self, context: List[str], response: str = ""):
-        ids = []
-        for text in context:
-            ids.extend(self.convert_text_to_ids(text))
-        if response:
-            ids.extend(self.convert_text_to_ids(response))
-        return ids
-
-    def _build(self, ids, shuffle=False):
-        """This method will be removed in the next release.
-
-        Args:
-            ids (Iterator[np.int32]): Iterator which generates
-                a np.int32 value at each time
-        """
-        window_length = self._block_size+1
-
-        # Example of dataset transformation
-        # In the example, {} means dataset, [] means batch
-        #
-        # Notes:
-        # - window makes new dataset consisting of several sub-Dataset
-        #
-        # Example:
-        #   init slice -> {1, 2, 3, 4, 5, 6, 7}
-        #   window     -> {{1, 2, 3}, {4, 5, 6}}
-        #   map batch  -> {{[1, 2, 3]}, {[4, 5, 6]}}
-        #   flat       -> {[1, 2, 3], [4, 5, 6]}
-        #   batch      -> {[[1, 2, 3], [4, 5, 6]]}
-        dataset = tf.data.Dataset.from_tensor_slices(ids)
-        dataset = dataset.window(window_length,
-                                 shift=self._block_size,
-                                 drop_remainder=True)
-        dataset = dataset.flat_map(lambda wd: wd.batch(window_length))
-        if shuffle:
-            dataset = dataset.shuffle(buffer_size)
-        dataset = dataset.batch(self._batch_size, drop_remainder=True)
-        dataset = dataset.map(lambda x: (x[:, :-1], x[:, 1:]))
-        dataset = dataset.prefetch(1)
-
-        return dataset
-
 
 class LineByLineDataset:
-    def __init__(self, max_len, encode_fn, buffer_size=10000):
-        self._max_len = max_len
-        self._encode_fn = encode_fn
+    @classmethod
+    def from_generator(cls, generator, encode_fn, max_len, batch_size=1, buffer_size=10000, shuffle=False):
+        def gen():
+            for text in generator():
+                yield np.array(encode_fn(text)[:max_len], dtype=np.int32)
 
-    def from_text_generator(self, generator, batch_size=1, buffer_size=10000, shuffle=False):
-        dataset = tf.data.Dataset.from_generator(self._gen_iter_ids(generator),
+        dataset = tf.data.Dataset.from_generator(gen,
                                                  output_types=tf.int32,
                                                  output_shapes=tf.TensorShape([None]))
         if shuffle:
@@ -99,46 +49,10 @@ class LineByLineDataset:
         # padded_shapes should be greater than the actual length
         dataset = dataset.padded_batch(batch_size=batch_size,
                                        padding_values=0,
-                                       padded_shapes=self._max_len,
+                                       padded_shapes=max_len,
                                        drop_remainder=True,
                                        )
         dataset = dataset.map(lambda x: (x[:, :-1], x[:, 1:]))
         dataset = dataset.prefetch(1)
 
         return dataset
-
-    def convert_text_to_ids(self, text: str):
-        return np.array(self._encode_fn(text)[:self._max_len], dtype=np.int32)
-
-    def _gen_iter_ids(self, text_generator):
-        def gen():
-            for text in text_generator():
-                yield self.convert_text_to_ids(text)
-        return gen
-
-    def convert_context_to_ids(self, context: List[str], response: str = ""):
-        ids = []
-        for text in context:
-            ids.extend(self._encode_fn(text))
-        if response:
-            ids.extend(self._encode_fn(response))
-        ids = np.array(ids[:self._max_len], dtype=np.int32)
-        return ids
-
-    def _build(self, ids, shuffle=False):
-        """This method will be removed in the next release.
-
-        Args:
-            ids (Iterator[List[np.int32]]): Iterator which generates
-                a List[lnp.int32] value at each time
-        """
-        dataset = tf.data.Dataset.from_generator(lambda: ids, tf.int32)
-        if shuffle:
-            dataset = dataset.shuffle(buffer_size)
-        dataset = dataset.padded_batch(batch_size=self._batch_size,
-                                       padding_values=0,
-                                       padded_shapes=self._max_len,
-                                       drop_remainder=True,
-                                       )
-        dataset = dataset.map(lambda x: (x[:, :-1], x[:, 1:]))
-        dataset = dataset.prefetch(1)
